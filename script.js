448365a1cc3d24e5ffe020eef4cde8e023f1e904
AnggaSaputra23 <autoruns18@gmail.com>
2025-06-20 15:02:08 +0800

    hay

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     event.preventDefault();
     alert('Pesan Berhasil Di Kirim Ke Zheta!! Terima kasih:D');
 });
+
+  const toggleBtn = document.querySelector(".theme-toggle");
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("night");
+  });
